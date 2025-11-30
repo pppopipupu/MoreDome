6e34bd823e50cb767ee54fca38595220a85addb0
pppopipupu <pppopipupu@gmail.com>
2025-11-30 18:48:41 +0800

    fix

--- src/moredome/content/domes/SlowdownDome.java
+++ src/moredome/content/domes/SlowdownDome.java
@@ -2,7 +2,12 @@ package moredome.content.domes;
 
 import arc.math.Mathf;
 import arc.util.Time;
+import mindustry.gen.Building;
 import mindustry.world.blocks.defense.OverdriveProjector;
+import mindustry.world.blocks.defense.turrets.BaseTurret;
+import mindustry.world.blocks.defense.turrets.Turret;
+import mindustry.world.blocks.units.RepairTower;
+import mindustry.world.blocks.units.RepairTurret;
 
 import static mindustry.Vars.indexer;
 
@@ -15,8 +20,10 @@ public class SlowdownDome extends OverdriveProjector {
         consumesPower = false;
         useTime = 0f;
     }
+
     public class SlowdownBuild extends OverdriveBuild {
         private float power = 0f;
+
         @Override
         public void updateTile() {
             smoothEfficiency = Mathf.lerpDelta(smoothEfficiency, efficiency, 0.08f);
@@ -29,9 +36,9 @@ public class SlowdownDome extends OverdriveProjector {
                 power = 0f;
 
                 indexer.eachBlock(this, realRange, other -> other.block.canOverdrive && other != this && other.power != null && other.block.consPower != null
-                          && other.shouldConsumePower && other.timeScale() <= 1.0f, other -> {
-                    other.applySlowdown(0.6f, reload + 1F);
-                    power += other.block.consPower.usage * other.timeScale();
+                        && other.shouldConsumePower && other.timeScale() == 1.0f && !(other instanceof RepairTower.RepairTowerBuild || other instanceof RepairTurret.RepairPointBuild), other -> {
+                    other.applySlowdown(0.6f * other.timeScale(), reload-1f);
+                    power += other.block.consPower.usage * other.timeScale() + 0.01f;
 
                 });
 
@@ -51,5 +58,5 @@ public class SlowdownDome extends OverdriveProjector {
             return power;
         }
     }
-    }
+}
 
